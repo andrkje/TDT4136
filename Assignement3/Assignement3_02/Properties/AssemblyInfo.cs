b681fe43b0274b3250986b73b520602466b5d6b7
Andreas Kjerstad <andreas.pi@hotmail.com>
2015-10-03 15:34:18 +0200

    Changed name

--- Assignement3/Assignement3_02/Properties/AssemblyInfo.cs
+++ Assignement3/Assignement3_02/Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Assignement3_02")]
+[assembly: AssemblyTitle("Assignement3")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
